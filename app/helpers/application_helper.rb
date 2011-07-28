2697a82720d120dd2ddc0017ffaa03772726af4e
Troy Will <troydwill@gmail.com>
2011-07-27 18:03:48 -0700

    application_helper.rb

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+  def html_header
+    link_1 = link_to "https://github.com/troywill/rails-ilc", "https://github.com/troywill/rails-ilc"
+    link_2 = link_to "http://troywill.com:5000", "http://troywill.com:5000"
+
+    return "#{link_1} | #{link_2}".html_safe
+  end
+end
