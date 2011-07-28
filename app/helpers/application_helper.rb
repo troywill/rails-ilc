12ae2f56c4d9e4846dca7abc22dba72fbba5e6c8
Troy Will <troydwill@gmail.com>
2011-07-27 19:22:38 -0700

    ../../helpers/application_helper.rb _form.html.erb "Categories label"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def html_header
     link_1 = link_to "https://github.com/troywill/rails-ilc", "https://github.com/troywill/rails-ilc"
-    link_2 = link_to "http://troywill.com:5000", "http://troywill.com:5000"
+    link_2 = link_to "http://troywill.com:7000", "http://troywill.com:7000"
 
     return "#{link_1} | #{link_2}".html_safe
   end
